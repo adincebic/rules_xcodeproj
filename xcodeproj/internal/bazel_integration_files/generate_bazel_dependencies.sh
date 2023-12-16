723b557714222ca86636e0610aa45e999149e08e
Adin Cebic <cebic.ad@gmail.com>
2023-12-16 09:33:54 +0100

    Replace deprecated experimental_remote_download_regex with remote_download_regex

--- xcodeproj/internal/bazel_integration_files/generate_bazel_dependencies.sh
+++ xcodeproj/internal/bazel_integration_files/generate_bazel_dependencies.sh
@@ -108,7 +108,7 @@ fi
 # Build
 
 build_pre_config_flags=(
-  "--experimental_remote_download_regex=.*\.indexstore/.*|.*\.a$|.*\.swiftdoc$|.*\.swiftmodule$|.*\.swiftsourceinfo$|.*\.swift$"
+  "--remote_download_regex=.*\.indexstore/.*|.*\.a$|.*\.swiftdoc$|.*\.swiftmodule$|.*\.swiftsourceinfo$|.*\.swift$"
 )
 
 apply_sanitizers=1
